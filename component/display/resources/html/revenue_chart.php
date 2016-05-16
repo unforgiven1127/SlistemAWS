dc0657e70c6b30c18fadb9e7d9a1c2f1fae754d8
unforgiven1127 <munir_anameric@hotmail.com>
2016-05-16 14:38:42 -0700

    Revenue chart

--- component/display/resources/html/revenue_chart.php
+++ component/display/resources/html/revenue_chart.php
@@ -12,16 +12,16 @@
 			<td>
 				<table class="revenue_table">
 					<tr>
-						<th class="text_center" colspan="6"><?php echo ucfirst($location); ?> - Individual Revenue Consultants <?php echo $year; ?></th>
+						<th style="font-size: large" class="text_center" colspan="6"><?php echo ucfirst($location); ?> - Individual Revenue Consultants <?php echo $year; ?></th>
 					</tr>
 					<tr>
-						<th style="width: 100px; height: 39px; font-size: 12pt;" class="text_center">Rank</th>
-						<th style="min-width: 200px; height: 39px; font-size: 12pt;" class="text_center">Name</th>
+						<th style="width: 100px; height: 39px; font-size: medium;" class="text_center">Rank</th>
+						<th style="min-width: 200px; height: 39px; font-size: medium;" class="text_center">Name</th>
 						<th style="height: 39px;" class="text_center"></th>
-						<th style="height: 39px; font-size: 12pt;" class="text_center">Signed</th>
-						<th style="height: 39px; font-size: 12pt;" class="text_center">Paid</th>
+						<th style="height: 39px; font-size: medium;" class="text_center">Signed</th>
+						<th style="height: 39px; font-size: medium;" class="text_center">Paid</th>
 						<!--<th class="text_center">Team</th>-->
-						<th style="width: 100px; height: 39px; font-size: 12pt;" class="text_center">Placed</th>
+						<th style="width: 100px; height: 39px; font-size: medium;" class="text_center">Placed</th>
 					</tr>
 
 					<?php
