63a2fcd31dc9ee3e4d503ed114eea5b4c04f6981
unforgiven1127 <munir_anameric@hotmail.com>
2016-05-30 22:16:50 -0700

    KPI total

--- component/display/resources/html/totals_chart.php
+++ component/display/resources/html/totals_chart.php
@@ -294,7 +294,7 @@
 	<?php $row_number_rank += 1; ?>
 	<?php endforeach ?>
 	<tr class="totals_table_footer"><td colspan="15">&nbsp;</td></tr>
-	<tr bgcolor="#FF0000">
+	<tr bgcolor="#58FAAC">
 		<td colspan="10" class="text_right">
 			Total :
 		</td>
