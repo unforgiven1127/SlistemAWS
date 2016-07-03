61753dc431bd6f6878f5abd9b0f1e01e58b26f2e
unforgiven1127 <munir_anameric@hotmail.com>
2016-07-03 16:16:06 -0700

    note activity

--- common/js/common.js
+++ common/js/common.js
@@ -1188,7 +1188,9 @@ function displaySalary(value)
 
     var listItems = oList.children('li:not(:first)').get();
 //console.log(listItems);
-//alert(psMode);
+alert(poElement);
+alert(psWay);
+alert(psMode);
     $(oList).animate({opacity: '0.2'}, function()
     {
         switch(psMode)
