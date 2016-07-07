b04ec1c652b6364c13645f0f07ebe013e4510498
unforgiven1127 <munir_anameric@hotmail.com>
2016-07-06 20:19:00 -0700

    login error

--- component/sl_candidate/resources/js/sl_candidate.js
+++ component/sl_candidate/resources/js/sl_candidate.js
@@ -1,7 +1,6 @@
 
 function toggleCandiTab(poElement, psIdToShow, psContainer, candidate_id)
 {
-  var serverName = window.location.host;
   //alert(serverName);
   //psIdToShow acilacak olan tab
   if(poElement)
@@ -15,6 +14,7 @@ function toggleCandiTab(poElement, psIdToShow, psContainer, candidate_id)
     {
       //alert(candidate_id);
       //alert(psIdToShow);
+      var serverName = window.location.host;
       var url = serverName+"/index.php5?uid=555-001&ppa=ppcl&ppt=candi&ppk="+candidate_id+"&pg=ajx&logType="+psIdToShow;
       alert(url);
       //var url = "/index.php5?uid=555-001&ppa=ppav&ppt=candi&pg=ajx&insertNewLog=contactDetail&ppk="+candidate_id;
