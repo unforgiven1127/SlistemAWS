6edffb18d573b4de11cdbf6e755c4d7567509133
unforgiven1127 <munir_anameric@hotmail.com>
2016-07-18 18:52:33 -0700

    position filled

--- update.php
+++ update.php
@@ -1,70 +1,29 @@
 <?php
 
-	//require_once('component/jobboard/jobboard.class.php5');
-	//require_once('component/taaggregator/resources/lib/encoding_converter.class.php5');
+    require_once('component/jobboard/jobboard.class.php5');
+    require_once('component/taaggregator/resources/lib/encoding_converter.class.php5');
 
-	//echo "Updates<br><br>";
-	//
-$arrayMulti = array();
-
-
-
-$array1=array( '	03-07-2012 17:39	','	354	','	279387	','	354	','	1	','	03-07-2012 17:39	','	Assessed in person	','	1	','	03-07-2012 17:39	');
-$array2=array( '	01-02-2010 19:46	','	354	','	272772	','	354	','	1	','	01-02-2010 19:46	','	Assessed in person	','	1	','	01-02-2010 19:46	');
-$array3=array( '	26-09-2008 13:03	','	354	','	262416	','	354	','	1	','	26-09-2008 13:03	','	Assessed in person	','	1	','	26-09-2008 13:03	');
-$array4=array( '	16-09-2008 20:26	','	354	','	259143	','	354	','	1	','	16-09-2008 20:26	','	Assessed in person	','	1	','	16-09-2008 20:26	');
-$array5=array( '	11-09-2008 12:22	','	363	','	257856	','	363	','	1	','	11-09-2008 12:22	','	Assessed in person	','	1	','	11-09-2008 12:22	');
-$array6=array( '	18-09-2008 17:23	','	333	','	257422	','	333	','	1	','	18-09-2008 17:23	','	Assessed in person	','	1	','	18-09-2008 17:23	');
-$array7=array( '	24-09-2008 12:25	','	270	','	254697	','	270	','	1	','	24-09-2008 12:25	','	Assessed in person	','	1	','	24-09-2008 12:25	');
-$array8=array( '	28-02-2013 17:27	','	155	','	252792	','	360	','	1	','	28-02-2013 17:27	','	Assessed in person	','	1	','	28-02-2013 17:27	');
-
-array_push($arrayMulti,$array1);
-array_push($arrayMulti,$array2);
-array_push($arrayMulti,$array3);
-array_push($arrayMulti,$array4);
-array_push($arrayMulti,$array5);
-array_push($arrayMulti,$array6);
-array_push($arrayMulti,$array7);
-array_push($arrayMulti,$array8);
-
-
-
-foreach ($arrayMulti as $key => $array)
-{
-	foreach ($array as $key => $value)
-	{
-		$array[$key] = TRIM($value);
-		echo $array[$key]." - ";
-	}
-	echo "<br><br>";
-}
-
-	/*JOBBOARD CONNECTION INFO*/
-	define('DB_NAME', 'jobboard');
-    define('DB_SERVER', '127.0.0.1');
-    define('DB_USER', 'jobboard');
-    define('DB_PASSWORD', 'KCd7C56XJ8Nud7uF');
-    /*JOBBOARD CONNECTION INFO*/
-
-    /*SLISTEM CONNECTION INFO*/
-	define('DB_NAME_SLISTEM','slistem');
+    define('DB_NAME_SLISTEM','slistem');
     define('DB_SERVER_SLISTEM', '127.0.0.1');
     define('DB_USER_SLISTEM', 'slistem');
     define('DB_PASSWORD_SLISTEM', 'smwXN2RTDm6Zz3hR');
-    /*SLISTEM CONNECTION INFO*/
 
-    /*JOBBOARD ISLEMLERI ICIN*/
     mysql_connect( DB_SERVER_SLISTEM, DB_USER_SLISTEM, DB_PASSWORD_SLISTEM) or die(mysql_error());
     mysql_select_db(DB_NAME_SLISTEM) or die(mysql_error());
 
 
-    /*foreach ($array as $key => $value)
+    $slistemQuery = " SELECT * FROM login l WHERE l.status = '1' ";
+    $slistemQuery = mysql_query($slistemQuery);
+
+    while($userData = mysql_fetch_assoc($slistemQuery))
     {
-    	$slistemQuery = "";
+        $pass = $userData['password'];
+        $user_id = $userData['loginpk'];
+        $pass_encrypted = sha1($pass);
 
-    	$positionData = mysql_query($slistemQuery);
-
-    }*/
+        echo $user_id." - ".$pass_encrypted;
+        echo "<br><br>";
+    }
 
 
     /*JOBBOARD ISLEMLERI ICIN*/
