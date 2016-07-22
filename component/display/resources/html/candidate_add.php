e543e319637538ab2903da9d1d289287214ccb0c
unforgiven1127 <munir_anameric@hotmail.com>
2016-07-21 17:50:30 -0700

    count complx search

--- component/display/resources/html/candidate_add.php
+++ component/display/resources/html/candidate_add.php
@@ -149,7 +149,7 @@
 							$rateNew = 1/$rate;
 							echo "<option value='".$currency."'";
 							echo $selected;
-							echo "title='Rate: 1' ".$currency." = ".$rateNew." &yen";
+							echo "title='Rate: 1 ".$currency." = ".$rateNew." &yen'";
 							echo $currency;
 							echo "</option>";
 						 } ?>
