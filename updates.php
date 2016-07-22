782aeca932bbc26ccf479181de4283edfc5db7ed
unforgiven1127 <munir_anameric@hotmail.com>
2016-07-21 19:52:21 -0700

    count complx search

--- updates.php
+++ updates.php
@@ -2,13 +2,26 @@
 
 //$sDate = date('Y-m-d H:i:s');
 //echo $sDate;
-
+header('Content-type: application/xml');
 $url = 'http://www.eltcalendar.com/rss.xml';
 
  $xmlstr = file_get_contents($url);
  $xmlcont = new SimpleXMLElement($xmlstr);
 
+
+var_dump($xmlstr);
+
+echo "<br><br>";
+echo "<br><br>TEST";
+echo "<br><br>";
+echo "<br><br>";
+
 var_dump($xmlcont);
+
+echo "<br><br>";
+echo "<br><br>TEST";
+echo "<br><br>";
+echo "<br><br>";
 
 foreach ($xmlcont as $key => $value)
 {
