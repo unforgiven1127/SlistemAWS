c2dc20b33224ca7011646c0e2787eda837bc4882
unforgiven1127 <munir_anameric@hotmail.com>
2016-07-25 21:52:38 -0700

    KPI

--- component/display/resources/html/totals_chart_ordered.php
+++ component/display/resources/html/totals_chart_ordered.php
@@ -89,11 +89,10 @@
 			</div>
 			<div class="stat_candi_info">
 			<?php
-			 foreach($allCanidatesArray[$arrayPosition][$value['user_id']] as $candidate_id => $data):
-			 	echo "<div>".var_dump($data)."</div>";
-				echo "<div>";
-
-					if(isset($data['set_meeting_info']))
+			 foreach($allCanidatesArray[$arrayPosition][$value['user_id']] as $candidate_id => $data): ?>
+				<div>
+				<?php
+					if(isset($data['setFlag']))
 					{
 						$url = $page_obj->getAjaxUrl('555-001', CONST_ACTION_VIEW, CONST_CANDIDATE_TYPE_CANDI, (int)$candidate_id);
 						echo "<a href='javascript: view_candi(".$url.")'>".$candidate_id."</a>";
@@ -103,9 +102,9 @@
 						$url = '';
 						echo "<a href='javascript:'>-</a>";
 					}
-
-				echo "</div>";
-			 endforeach ?>
+				?>
+				</div>
+			<?php endforeach ?>
 			</div>
 		</td>
 		<td>
