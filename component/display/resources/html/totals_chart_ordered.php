1b0cf72723ca8af35c06c011648d12afb45e3109
unforgiven1127 <munir_anameric@hotmail.com>
2016-07-26 21:05:28 -0700

    KPI

--- component/display/resources/html/totals_chart_ordered.php
+++ component/display/resources/html/totals_chart_ordered.php
@@ -738,7 +738,7 @@ $(document).ready(function(){
 		});
 
 		$('.stat_holder').click(function() {
-			var newClass = $(this).id;
+			var newClass = $(this).attr("id");
 			alert(newClass);
 			$('.'+newClass).toggle();
 			//var sibling_obj_size = $($(this).siblings().get(0)).children().length;
