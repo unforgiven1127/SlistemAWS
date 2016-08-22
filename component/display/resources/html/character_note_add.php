66e2c882ffc23cd35024fd35c15eea088cebad8a
unforgiven1127 <munir_anameric@hotmail.com>
2016-08-22 14:14:06 -0700

    character notes

--- component/display/resources/html/character_note_add.php
+++ component/display/resources/html/character_note_add.php
@@ -4,12 +4,18 @@
 	    /*margin: 0 auto !important;*/
 	    /*margin-top:15px !important;*/
 	    margin-left: -100px !important;
-	    padding-top:20px !important;
+	    padding-top: 20px !important;
 	}
 
 	.titles{
 		text-align: center !important;
 		background-color: #EEEEEE !important;
+		width:306px !important;
+		height:20px !important;
+	}
+
+	.inputs{
+		width:306px !important;
 	}
 
 </style>
@@ -17,26 +23,26 @@
 
 <table class="box" align="center">
 	<tr>
-		<td class='titles'><p>Personality & Communication</p></td>
-		<td class='titles'><p>Career Expertise – Present, Past & Future</p></td>
-		<td class='titles'><p>Education & Training</p></td>
+		<td><p class='titles'>Personality & Communication</p></td>
+		<td><p class='titles'>Career Expertise – Present, Past & Future</p></td>
+		<td><p class='titles'>Education & Training</p></td>
 	<tr>
 	<tr>
-		<td><textarea></textarea></td>
-		<td><textarea></textarea></td>
-		<td><textarea></textarea></td>
+		<td><textarea class='inputs'></textarea></td>
+		<td><textarea class='inputs'></textarea></td>
+		<td><textarea class='inputs'></textarea></td>
 	</tr>
 </table>
 
 <table class="box" align="center">
 	<tr>
-		<td class='titles'><p>Move – Reason & Timing</p></td>
-		<td class='titles'><p>Compensation Breakdown & Desire</p></td>
-		<td class='titles'><p>Companies – Recently Met & Introduced</p></td>
+		<td><p class='titles'>Move – Reason & Timing</p></td>
+		<td><p class='titles'>Compensation Breakdown & Desire</p></td>
+		<td><p class='titles'>Companies – Recently Met & Introduced</p></td>
 	<tr>
 	<tr>
-		<td><textarea></textarea></td>
-		<td><textarea></textarea></td>
-		<td><textarea></textarea></td>
+		<td><textarea class='inputs'></textarea></td>
+		<td><textarea class='inputs'></textarea></td>
+		<td><textarea class='inputs'></textarea></td>
 	</tr>
 </table>
