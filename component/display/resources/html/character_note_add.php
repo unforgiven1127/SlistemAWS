62b52511185ab8e3244db41eb05648f29d5fde85
unforgiven1127 <munir_anameric@hotmail.com>
2016-08-24 21:17:02 -0700

    char note

--- component/display/resources/html/character_note_add.php
+++ component/display/resources/html/character_note_add.php
@@ -40,7 +40,7 @@
 		{
 			$('#personality_note').css({'border-color':'red'});
 			//$('#personality_note').css('borderWidth', '2px');
-			$('#personality_note').css('box-shadow', '10px 10px 5px red');
+			$('#personality_note').css('box-shadow', '5px 5px 2px red');
 		}
 		if(career_length < 25)
 		{
