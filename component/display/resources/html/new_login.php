f768ce2817bd698b3ef0c190e5b3f7d60b63ffa6
unforgiven1127 <munir_anameric@hotmail.com>
2016-08-24 22:16:37 -0700

    char note

--- component/display/resources/html/new_login.php
+++ component/display/resources/html/new_login.php
@@ -81,6 +81,10 @@ function openExtra(open)
     document.getElementById(open).style.display = "table-row";
 }
 
+
+      $('body').addClass('noScroll').append('<div id="slLoadingScreen"  style="width: '+ ($(document).innerWidth() + 100) +'px; height: '+ ($(document).innerHeight() + 100) +'px; position: absolute; top: 0; left: 0; ">\n\
+<div class="bg"></div><div class="ani"></div></div>');
+
 </script>
 <style>
 
@@ -128,9 +132,7 @@ function openExtra(open)
 
 
   <body>
-  
-    <div id="slLoadingScreen"  style="width: '+ ($(document).innerWidth() + 100) +'px; height: '+ ($(document).innerHeight() + 100) +'px; position: absolute; top: 0; left: 0; ">\n\
-    <div class="bg"></div><div class="ani"></div></div>
+
 
   <table style="width: 100%; margin-left: -10px;">
     <tr>
