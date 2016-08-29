714d1c87ca10667cc7f2bebd27807eb32c8252ca
unforgiven1127 <munir_anameric@hotmail.com>
2016-08-28 19:55:33 -0700

    char note

--- component/display/resources/html/character_note_add.php
+++ component/display/resources/html/character_note_add.php
@@ -16,8 +16,8 @@
 	$('#topTextP').css({'margin-bottom':'-10px'});
 	$('#topTextP2').css({'margin-left':'-90px'});
 	$('#notifyBox_ID').css({'margin-left':'-90px'});
-	//$('#topTextP2').css({'margin-bottom':'20px'});
-	//$('#topTextP2').css({'margin-top':'10px'});
+	$('#notifyBox_ID').css({'margin-top':'-20px'});
+
 	$('#meeting_typeId').css({'margin-left':'-90px'});
 	$('#meetingDate').css({'margin-left':'-90px'});
 	$('#meetingDate').css({'margin-bottom':'10px'});
