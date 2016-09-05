8d910aba5eac5f707a68dcc359b38dcde016790e
unforgiven1127 <munir_anameric@hotmail.com>
2016-09-04 19:19:08 -0700

    test area

---
+++ component/display/resources/html/delete_company_page.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>Sl[i]stem by Slate</title>
+
+
+<style>
+
+
+</style>
+
+<script type="text/javascript">
+
+
+</script>
+
+  </head>
+
+
+  <body>
+
+  <script language="javascript">
+
+  </script>
+
+  <?php if(isset($header)){ echo $header; } ?>
+  <table>
+    <tr>
+      <td colspan="2" valign="top">
+        Confirm deleting company #123456
+      </td>
+    </tr>
+    <tr>
+      <td>
+        Move employees to:[company id]
+      </td>
+      <td>
+        <input type="text" name="company_id"  id="company_id">
+      </td>
+    </tr>
+  </table>
+
+
+  </body>
+
+</html>
