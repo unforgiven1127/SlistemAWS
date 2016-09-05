6373c81868a4ba6a1681475a26172c89a6864e22
unforgiven1127 <munir_anameric@hotmail.com>
2016-09-04 19:25:45 -0700

    test area

--- component/display/resources/html/delete_company_page.php
+++ component/display/resources/html/delete_company_page.php
@@ -27,7 +27,14 @@
   <?php if(isset($header)){ echo $header; } ?>
   <table>
     <tr>
-      <td colspan="2" valign="top">
+      <td  colspan="2" valign="top">
+        <div style=" font-size:15px; width: 300px; color:#585858; font-weight: bold; " class="alert alert-danger" role="alert">
+                Warning you are deleting company #123456!
+        </div>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="2">
         Confirm deleting company #123456
       </td>
     </tr>
