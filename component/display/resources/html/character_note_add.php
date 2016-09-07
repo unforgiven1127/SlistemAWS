a24b827d8bd374ff204b0a8101df82274244965e
unforgiven1127 <munir_anameric@hotmail.com>
2016-09-06 19:00:39 -0700

    company delete merge

--- component/display/resources/html/character_note_add.php
+++ component/display/resources/html/character_note_add.php
@@ -178,39 +178,55 @@
 		/*margin-top: -10px !important;*/
 	}
 
+	.alert-danger {
+	  background-image: -webkit-linear-gradient(top, #f2dede 0%, #e7c3c3 100%);
+	  background-image:      -o-linear-gradient(top, #f2dede 0%, #e7c3c3 100%);
+	  background-image: -webkit-gradient(linear, left top, left bottom, from(#f2dede), to(#e7c3c3));
+	  background-image:         linear-gradient(to bottom, #f2dede 0%, #e7c3c3 100%);
+	  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#fff2dede', endColorstr='#ffe7c3c3', GradientType=0);
+	  background-repeat: repeat-x;
+	  border-color: #dca7a7;
+	}
+
+	.alert {
+	  text-shadow: 0 1px 0 rgba(255, 255, 255, .2);
+	  -webkit-box-shadow: inset 0 1px 0 rgba(255, 255, 255, .25), 0 1px 2px rgba(0, 0, 0, .05);
+	          box-shadow: inset 0 1px 0 rgba(255, 255, 255, .25), 0 1px 2px rgba(0, 0, 0, .05);
+	}
+
 
 </style>
 
 <table class="box3" align="center">
 	<tr>
-		<td>What does the candidate do -what is his profession?</td>
+		<td class="alert alert-danger">What does the candidate do -what is his profession?</td>
 	</tr>
 	<tr>
-		<td>What is his scope of experience in this profession? Is he specific or broad?</td>
+		<td class="alert alert-danger">What is his scope of experience in this profession? Is he specific or broad?</td>
 	</tr>
 	<tr>
-		<td>Is the candidate intelligent and able to solve problems or not?</td>
+		<td class="alert alert-danger">Is the candidate intelligent and able to solve problems or not?</td>
 	</tr>
 	<tr>
-		<td>Can the candidate manage problems or not?</td>
+		<td class="alert alert-danger">Can the candidate manage problems or not?</td>
 	</tr>
 	<tr>
-		<td>What does the candidate hope to accomplish before he moves to the next position?</td>
+		<td class="alert alert-danger">What does the candidate hope to accomplish before he moves to the next position?</td>
 	</tr>
 	<tr>
-		<td>What is his vision for his future? What is the next step for him if and when he moves?</td>
+		<td class="alert alert-danger">What is his vision for his future? What is the next step for him if and when he moves?</td>
 	</tr>
 	<tr>
-		<td>How does he look? Is he confident and articulate? Does he have a powerful presence?</td>
+		<td class="alert alert-danger">How does he look? Is he confident and articulate? Does he have a powerful presence?</td>
 	</tr>
 	<tr>
-		<td>Can he manage a team and is he ambitious?</td>
+		<td class="alert alert-danger">Can he manage a team and is he ambitious?</td>
 	</tr>
 	<tr>
-		<td>Is he met (not placeable), notable (placeable) or top shelf (Absolutely placeable)?</td>
+		<td class="alert alert-danger">Is he met (not placeable), notable (placeable) or top shelf (Absolutely placeable)?</td>
 	</tr>
 	<tr>
-		<td>Focus on your candidate\'s career process and placability when you are writing comments. </td>
+		<td class="alert alert-danger">Focus on your candidate\'s career process and placability when you are writing comments. </td>
 	</tr>
 
 </table>
