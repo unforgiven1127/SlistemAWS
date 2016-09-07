095ee8497848a248f663b36f88102794b7510dad
unforgiven1127 <munir_anameric@hotmail.com>
2016-09-06 21:53:13 -0700

    character assessment

--- component/display/resources/html/character_note_add.php
+++ component/display/resources/html/character_note_add.php
@@ -76,12 +76,14 @@
 		if(personality_length < 25)
 		{
 			$('#personality_note').css({'border-color':'red'});
-			$('#personality_note').css({'border-width':'3px'});
+			$('#personality_note').css({'border-left-width':'3px'});
+			$('#personality_note').css({'border-right-width':'3px'});
 		}
 		else
 		{
 			$('#personality_note').css({'border-color':'grey'});
-			$('#personality_note').css({'border-width':'1px'});
+			$('#personality_note').css({'border-left-width':'1px'});
+			$('#personality_note').css({'border-right-width':'2px'});
 		}
 
 		if(career_length < 25)
