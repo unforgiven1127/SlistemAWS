ce1f480d7ae0f008ae4078063d2197b77145f2dc
unforgiven1127 <munir_anameric@hotmail.com>
2016-09-12 14:09:51 -0700

    complex search

--- component/form/resources/js/jquery.tokeninput.js
+++ component/form/resources/js/jquery.tokeninput.js
@@ -49,8 +49,15 @@ var DEFAULT_SETTINGS = {
 
     resultsFormatter: function(item)
     {
-      alert("<li class='"+item['class_result']+" autocomp_lvl_"+item.level+"'>"+item[this.propertyToSearch]+ "</li>");
-      return "<li class='"+item['class_result']+" autocomp_lvl_"+item.level+"'>"+item[this.propertyToSearch]+ "</li>";
+      if(item.level == '1')
+      {
+        return "<li disabled class='"+item['class_result']+" autocomp_lvl_"+item.level+"'>"+item[this.propertyToSearch]+ "</li>";
+      }
+      else
+      {
+        return "<li class='"+item['class_result']+" autocomp_lvl_"+item.level+"'>"+item[this.propertyToSearch]+ "</li>";
+      }
+      
     },
     tokenFormatter: function(item) { return "<li class='"+item['class_selected']+" autocomp_lvl_"+item.level+"'><p>" + item[this.propertyToSearch] + "</p></li>"; },
 
