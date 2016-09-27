86c079ac0932b1e688ba7f0afcfb40d8b024902a
unforgiven1127 <munir_anameric@hotmail.com>
2016-09-26 21:31:07 -0700

    test

--- common/js/common.js
+++ common/js/common.js
@@ -271,7 +271,7 @@ function AjaxRequest(psUrl, psLoadingScreen, psFormToSerialize, psZoneToRefresh,
       scriptCharset: "utf-8" ,
       contentType: "application/x-www-form-urlencoded; charset=UTF-8",
       context: document.body,
-      async: pbSynch,
+      async: false,
       dataType: "JSON",
       success: function(oJsonData)
       {
