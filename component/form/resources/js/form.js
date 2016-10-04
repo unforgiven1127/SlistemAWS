97c2e8359aedfb70d6e23fdf1f146bfda718f60f
unforgiven1127 <munir_anameric@hotmail.com>
2016-10-04 14:45:42 -0700

    company dupli

--- component/form/resources/js/form.js
+++ component/form/resources/js/form.js
@@ -28,7 +28,7 @@ function ShowDialogBox(title, content, btn1text, btn2text, functionText, paramet
         title: title,
         modal: true,
         width: '500px',
-        height: '900px',
+        height: 'auto',
         bgiframe: false,
         hide: { effect: 'scale', duration: 400 },
 
