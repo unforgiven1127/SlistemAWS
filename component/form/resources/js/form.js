8b390b8d0e636b32b2b237a272552b4fcb488cae
unforgiven1127 <munir_anameric@hotmail.com>
2016-10-03 21:01:33 -0700

    company dupli

--- component/form/resources/js/form.js
+++ component/form/resources/js/form.js
@@ -2,7 +2,7 @@
  * Comment
  */
 
-$('button[name=Save_company]').type = 'button';
+$('input[name=Save_company]').type = 'button';
 
 function ShowDialogBox(title, content, btn1text, btn2text, functionText, parameterList) {
                 var btn1css;
