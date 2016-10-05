c243194b95d6a3b3d955582c0bfc1df667eaa999
unforgiven1127 <munir_anameric@hotmail.com>
2016-10-04 17:21:31 -0700

    company dupli

--- updates.php
+++ updates.php
@@ -3,7 +3,7 @@
 //$sDate = date('Y-m-d H:i:s');
 //echo $sDate;
 
-$gelen = $_SERVER['HTTP_USER_AGENT'];
+/*$gelen = $_SERVER['HTTP_USER_AGENT'];
 
 if (strpos($gelen, 'Firefox') !== false)
 {
@@ -12,7 +12,42 @@ if (strpos($gelen, 'Firefox') !== false)
 else
 {
   echo 'Chrome vs';
-}
+}*/
+$sDate = date('Y-m-d H:i:s');
+$to = "munir@slate-ghc.com, rkiyamu@slate.co.jp";
+$subject = "Test email";
+
+$message = "
+<html>
+<head>Date&Time:
+".$sDate."
+<title>Test email</title>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>
+<tr>
+<td>John</td>
+<td>Testova</td>
+</tr>
+</table>
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <slistem@slate.co.jp>' . "\r\n";
+$headers .= 'Cc: munir_anameric@hotmail.com' . "\r\n";
+
+mail($to,$subject,$message,$headers);
 
 	/*define('DB_NAME_SLISTEM','slistem');
     define('DB_SERVER_SLISTEM', '127.0.0.1');
