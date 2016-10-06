0f62387a234232b9e7ff276a4b71c5baa67b982f
unforgiven1127 <munir_anameric@hotmail.com>
2016-10-05 21:51:00 -0700

    Revenue board

--- component/display/resources/html/totals_chart_ordered.php
+++ component/display/resources/html/totals_chart_ordered.php
@@ -23,8 +23,7 @@
 	    if(loopFlag == true || loopFlag == 'true')
 		{
 			document.getElementById('getKpiForm').setAttribute("style","display:none;");
-			document.getElementById('body').setAttribute("style","margin-top:-40px;");
-			document.getElementById('body').setAttribute("style","font-size:200% !important;");
+			document.getElementById('body').setAttribute("style","margin-top:-40px;font-size:200% !important;");
 			//$('#totals_table_id').css('margin-top','-48px;');
 			//alert('GIZLEME SONRASI ALERT');
 		}
