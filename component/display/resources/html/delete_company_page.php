790e662d6b089a1198bd282ae5f84826f0c857af
unforgiven1127 <munir_anameric@hotmail.com>
2016-10-17 13:48:38 -0700

    company autocomp

--- component/display/resources/html/delete_company_page.php
+++ component/display/resources/html/delete_company_page.php
@@ -54,7 +54,7 @@
   </head>
 
 
-  <body id='deleteCompany'>
+  <body>
 
   <script language="javascript">
 
@@ -93,7 +93,7 @@
   <table style='width:100%;'>
     <tr>
       <td align="right" style='padding-top: 30px; padding-right: 55px;'>
-        <button onclick="$('.ui-dialog').remove();" type="button" class="log-btn_" >No</button>
+        <button onclick="$('.ui-dialog').remove();" type="button" class="log-btn_ ui-dialog-titlebar-close" >No</button>
         <button onclick="
           var selctedCompany = document.getElementsByName('company_id');
           $('.ui-dialog').remove();
