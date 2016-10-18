f96adc020d83b97ab4a440f8213795ee293fd68c
unforgiven1127 <munir_anameric@hotmail.com>
2016-10-17 19:05:10 -0700

    log errors

--- updates.php
+++ updates.php
@@ -14,7 +14,7 @@ else
   echo 'Chrome vs';
 }*/
 $sDate = date('Y-m-d H:i:s');
-$to = "munir@slate-ghc.com, rkiyamu@slate.co.jp";
+$to = "munir@slate-ghc.com";
 $subject = "Test email";
 
 $message = "
@@ -48,7 +48,7 @@ $headers .= 'From: <slistem@slate.co.jp>' . "\r\n";
 $headers .= 'Cc: munir_anameric@hotmail.com' . "\r\n";
 
 mail($to,$subject,$message,$headers);
-echo 'mail send...';
+
 	/*define('DB_NAME_SLISTEM','slistem');
     define('DB_SERVER_SLISTEM', '127.0.0.1');
     define('DB_USER_SLISTEM', 'slistem');
