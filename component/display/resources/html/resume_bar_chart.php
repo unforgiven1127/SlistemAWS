512c20ca21b5a7a13dad021b7456a8410935ace9
unforgiven1127 <munir_anameric@hotmail.com>
2016-10-20 20:40:14 -0700

    noktali virgul

--- component/display/resources/html/resume_bar_chart.php
+++ component/display/resources/html/resume_bar_chart.php
@@ -34,7 +34,7 @@ $(function () {
                 zIndex:4,
                 label:{
                         text:'CUSTOM TARGET',
-                        align: 'center'
+                        align: 'middle'
                       }
             }]
         },
