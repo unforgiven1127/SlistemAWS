24820acdad7a35fa10f1d9874a1882a65da9341b
unforgiven1127 <munir_anameric@hotmail.com>
2016-10-24 13:53:15 -0700

    new graphs

--- component/display/resources/html/candidates_met_bar_chart.php
+++ component/display/resources/html/candidates_met_bar_chart.php
@@ -36,7 +36,7 @@ $(function () {
             text: 'Candidates Met 01.05.2016 to Present'
         },
         xAxis: {
-            categories: [<?php foreach($data as $key => $value){ echo "\"$value\""; } ?>]
+            categories: [<?php echo join($data, ',') ?>]
             //categories: ['R.Pedersen |19|', 'P.Thai |13|', 'M.Moir |10|', 'Y.Takagi |4|', 'G.Young |2|',]
         },
         yAxis: {
