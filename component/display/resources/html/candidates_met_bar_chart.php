7936997d3937d21481599ee01125919e6f26abe4
unforgiven1127 <munir_anameric@hotmail.com>
2016-10-23 20:00:44 -0700

    noktali virgul

--- component/display/resources/html/candidates_met_bar_chart.php
+++ component/display/resources/html/candidates_met_bar_chart.php
@@ -24,6 +24,7 @@ $(function () {
             categories: ['R.Pedersen |19|', 'P.Thai |13|', 'M.Moir |10|', 'Y.Takagi |4|', 'G.Young |2|']
         },
         yAxis: {
+            tickInterval:2,
             min: 0,
             title: {
                 text:'',
