0bed0d09374edaa546c6d55bcc63ca4ccc9f155c
unforgiven1127 <munir_anameric@hotmail.com>
2016-10-24 16:00:42 -0700

    new graphs

--- component/display/resources/html/candidates_met_bar_chart.php
+++ component/display/resources/html/candidates_met_bar_chart.php
@@ -35,7 +35,7 @@ $(function () {
     var user_formatted_array = "<?php echo $tes; ?>";
     alert(user_formatted_array);
 
-    var asd = "asdasd,asdasasd,asdsdasad";
+    var asd = "'asda'sd,asdasasd,asdsdasad";
 
     var user_130_count = parseInt("<?php echo $new_candidate_met[130]['count']; ?>");
     var user_276_count = parseInt("<?php echo $new_candidate_met[276]['count']; ?>");
