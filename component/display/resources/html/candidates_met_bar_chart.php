2317de590bad40a156c7fd315559ec5090c81f22
unforgiven1127 <munir_anameric@hotmail.com>
2016-10-24 15:03:36 -0700

    new graphs

--- component/display/resources/html/candidates_met_bar_chart.php
+++ component/display/resources/html/candidates_met_bar_chart.php
@@ -13,11 +13,9 @@
 <script type="text/javascript">
 $(function () {
 
-    var formatted_json = "<?php echo $formatted_json; ?>";
-    var count_json = "<?php echo $count_json; ?>";
+    var new_candidate_met_json = "<?php echo $new_candidate_met_json; ?>";
 
-    alert(formatted_json);
-    alert(count_json);
+    alert(new_candidate_met_json);
 
     var user_130 = "<?php echo $new_candidate_met[130]['formatted']; ?>";
     var user_276 = "<?php echo $new_candidate_met[276]['formatted']; ?>";
