1478a6aaa702b7b76150cc6bc8f592ef78c715f3
unforgiven1127 <munir_anameric@hotmail.com>
2016-10-25 20:02:06 -0700

    new graphs

--- component/display/resources/html/candidate_in_play_bar_chart.php
+++ component/display/resources/html/candidate_in_play_bar_chart.php
@@ -75,17 +75,26 @@ $(function () {
             }
         }],
         legend: {
-            reversed: true,
+            //reversed: true,
             verticalAlign: 'top',
             itemMarginTop: 50,
             itemMarginBottom: 50
         },
         plotOptions: {
-            series: {
-                stacking: 'normal'
+            bar: {
+                dataLabels: {
+                    enabled: true
+                }
             }
         },
-        series: [ {
+        series: [
+        {
+            name: 'Candidate in play',
+            color: 'rgba(28, 139, 176,0.5)',//blue
+            opacity: '.4',
+            data: inplay_count
+        },
+        {
             name: 'Resume sent',
             style: {
                         fontSize: '20px',
@@ -94,11 +103,6 @@ $(function () {
             color: 'rgba(179, 0, 0,0.5)',//red
             opacity: '.4',
             data: inplay_rsc
-        }, {
-            name: 'Candidate in play',
-            color: 'rgba(28, 139, 176,0.5)',//blue
-            opacity: '.4',
-            data: inplay_count
         }]
 
     });
