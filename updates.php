146e605049d7ee2dedfe62ca58748d963882b218
unforgiven1127 <munir_anameric@hotmail.com>
2016-11-10 23:24:37 -0800

    db

--- updates.php
+++ updates.php
@@ -1,5 +1,44 @@
 <?php
 
+$filename = 'churc.sql';
+// MySQL host
+$mysql_host = 'localhost';
+// MySQL username
+$mysql_username = 'nopasss';
+// MySQL password
+$mysql_password = 'smwXN2RTDm6Zz3hR';
+// Database name
+$mysql_database = 'slistem';
+
+// Connect to MySQL server
+mysql_connect($mysql_host, $mysql_username, $mysql_password) or die('Error connecting to MySQL server: ' . mysql_error());
+// Select database
+mysql_select_db($mysql_database) or die('Error selecting MySQL database: ' . mysql_error());
+
+// Temporary variable, used to store current query
+$templine = '';
+// Read in entire file
+$lines = file($filename);
+// Loop through each line
+foreach ($lines as $line)
+{
+// Skip it if it's a comment
+if (substr($line, 0, 2) == '--' || $line == '')
+    continue;
+
+// Add this line to the current segment
+$templine .= $line;
+// If it has a semicolon at the end, it's the end of the query
+if (substr(trim($line), -1, 1) == ';')
+{
+    // Perform the query
+    mysql_query($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . mysql_error() . '<br /><br />');
+    // Reset temp variable to empty
+    $templine = '';
+}
+}
+ echo "Tables imported successfully";
+
 //$sDate = date('Y-m-d H:i:s');
 //echo $sDate;
 
@@ -13,7 +52,7 @@ else
 {
   echo 'Chrome vs';
 }*/
-$sDate = date('Y-m-d H:i:s');
+/*$sDate = date('Y-m-d H:i:s');
 $to = "munir@slate-ghc.com";
 $subject = "Test email";
 
